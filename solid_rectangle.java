4d6860935dda499335f35414de32b2d55dac92a9
Jaya Pandey <92687204+Jaya6760@users.noreply.github.com>
2023-06-21 01:09:49 +0530

    Add files via upload

---
+++ solid_rectangle.java
@@ -0,0 +1,13 @@
+public class solid_rectangle {
+    public static void main(String[] args){
+        int c = 5;
+        int r = 4;
+        for(int i = 0 ; i < r; i++){
+            for(int j = 1; j < c; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println("*");
+        }
+        System.out.println(" ");
+    }
+}
